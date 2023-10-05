315654c1171ac3208366c5f1d7cb4f911845bb55
Shurkel <70747657+Shurkel@users.noreply.github.com>
2023-10-05 19:28:05 +0300

    dumnezeu spune da

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,12 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  public toastButtons = [
+  private lastDismissedRole: string | null = null; // Track the role of the last dismissed alert
+
+  public alertButtons = [
     {
-      text: 'Dismiss',
+      text: 'Cancel',
       role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+        this.lastDismissedRole = 'cancel'; // Update the last dismissed role
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+        this.lastDismissedRole = 'confirm'; // Update the last dismissed role
+      },
     },
   ];
-  constructor() {}
 
+  constructor() {
+    // Call the showAlert method automatically every 5 seconds
+    setInterval(() => this.showAlert(), 5000);
+  }
+
+  showAlert() {
+    // Trigger the alert only if the last dismissed role was not 'cancel'
+    if (this.lastDismissedRole !== 'cancel') {
+      const alert = document.querySelector('ion-alert');
+      if (alert) {
+        alert.present(); // Display the alert
+      }
+    }
+  }
+
+  setResult(ev: any) {
+    console.log(`Dismissed with role: ${ev.detail.role}`);
+    this.lastDismissedRole = ev.detail.role; // Update the last dismissed role
+  }
 }
