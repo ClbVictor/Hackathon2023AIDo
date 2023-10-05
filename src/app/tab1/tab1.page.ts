f10cae45f48572307460a3fbbd94476c588d6c2a
Shurkel <70747657+Shurkel@users.noreply.github.com>
2023-10-05 17:55:34 +0300

    da

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  public toastButtons = [
+    {
+      text: 'Dismiss',
+      role: 'cancel',
+    },
+  ];
   constructor() {}
 
 }
